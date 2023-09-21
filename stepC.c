3a1a9f9e75388a64f081247de3e3ccf516ac149d
mateoquaglia <117778104+mateoquaglia@users.noreply.github.com>
2023-09-20 22:45:23 -0300

    Update stepC.c

--- stepC.c
+++ stepC.c
@@ -1,79 +1,47 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/sysinfo.h>
-#include <sys/vfs.h>
-#include <time.h>
-#include <dirent.h>
-#include <errno.h>
+#include "StepC.h"
 
-// Función para obtener la cantidad de memoria configurada en el hardware
-unsigned long long getMemoryTotal() {
-    struct sysinfo si;
-    if (sysinfo(&si) != 0) {
-        perror("Error al obtener información del sistema");
-        exit(EXIT_FAILURE);
-    }
-    return si.totalram * si.mem_unit;
-}
+/**
+información del disco 
+ */
+void memstats(int a, int b)
+{
+  char path_avg[] = "/proc/loadavg";
+  char path_mem[] = "/proc/meminfo";
+  char path_disk[] = "/proc/diskstats";
 
-// Función para obtener la cantidad de memoria disponible
-unsigned long long getMemoryAvailable() {
-    struct sysinfo si;
-    if (sysinfo(&si) != 0) {
-        perror("Error al obtener información del sistema");
-        exit(EXIT_FAILURE);
-    }
-    return si.freeram * si.mem_unit;
-}
+  float min;
+  char *memtotal, *memavailable, *peticiones;
+  int total, available, petitions;
 
-// Función para obtener el número de peticiones a disco realizadas
-unsigned long long getDiskRequests() {
-    FILE *diskstatsFile = fopen("/proc/diskstats", "r");
-    if (diskstatsFile == NULL) {
-        perror("Error al abrir /proc/diskstats");
-        exit(EXIT_FAILURE);
-    }
+  int veces = b/a;
 
-    unsigned long long diskRequests = 0;
-    char line[256];
-    while (fgets(line, sizeof(line), diskstatsFile) != NULL) {
-        // Analizar el archivo /proc/diskstats para obtener información de peticiones a disco
-        // Aquí debes implementar la lógica para analizar el archivo y calcular las peticiones a disco.
-    }
+  for(int i = 0; i < veces; i++)
+  {
+    FILE *disk = fopen(path_disk,"r");
 
-    fclose(diskstatsFile);
-    return diskRequests;
-}
+    char linea[1000];
 
-int main(int argc, char *argv[]) {
-    if (argc != 4 || strcmp(argv[1], "-l") != 0) {
-        printf("Uso: rdproc -l <interval> <duration>\n");
-        return EXIT_FAILURE;
-    }
+    fclose(disk);
 
-    int interval = atoi(argv[2]);
-    int duration = atoi(argv[3]);
-    time_t startTime = time(NULL);
+    sscanf(peticiones,"%*d %*d %*s %d", &petitions);
 
-    while (1) {
-        time_t currentTime = time(NULL);
-        if (currentTime - startTime >= duration) {
-            break;
-        }
+    FILE *mem = fopen(path_mem,"r");
+    memtotal = obtenerInfo("MemTotal", mem);
+    memavailable = obtenerInfo("MemAvailable", mem);
+    fclose(mem);
 
-        unsigned long long diskRequests = getDiskRequests();
-        unsigned long long memoryTotal = getMemoryTotal();
-        unsigned long long memoryAvailable = getMemoryAvailable();
-        double loadAverage = getloadavg(NULL, 1);  // Obtiene el promedio de carga de 1 minuto
+    sscanf(memtotal,"%d", &total);
+    sscanf(memavailable,"%d", &available);
 
-        printf("Peticiones a disco: %llu\n", diskRequests);
-        printf("Memoria disponible / total: %llu / %llu\n", memoryAvailable, memoryTotal);
-        printf("Promedio de carga en el último minuto: %.2lf\n", loadAverage);
+    FILE *avg = fopen(path_avg,"r");
+    fscanf(avg, "%f", &min);
+    fclose(avg);
 
-        sleep(interval);
-    }
-
-    return 0;
+    printf("Peticiones a disco: %d\n", petitions);
+    printf("Memoria disponible / total: %d / %d\n",
+          available, total);
+    printf("Promedio de carga en el último minuto: %.2f\n", min);
+    printf("[Pausa de %d segundos]\n\n", a);
+    sleep(a);
+  }
 }
